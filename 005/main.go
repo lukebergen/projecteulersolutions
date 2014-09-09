112319b72985bf6482d09d2f335dfdac26c49b1a
Luke Bergen <LukeABergen@gmail.com>
2014-09-09 14:59:27 -0400

    Adding solution for problem 5

--- 005/main.go
+++ 005/main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("solution")
+	var result int64 = 20
+	for ; !divisible(result); result += 20 {
+	}
+	fmt.Println("result: %d", result)
+}
+
+func divisible(n int64) bool {
+	divisors := []int64{11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	l := len(divisors)
+
+	for i := 0; i < l; i++ {
+		if n%divisors[i] != 0 {
+			return false
+		}
+	}
+	return true
 }
--- 005/main_test.go
+++ 005/main_test.go
@@ -7,3 +7,16 @@ func BenchmarkMain(b *testing.B) {
 		main()
 	}
 }
+
+func TestDivisible(t *testing.T) {
+	if divisible(17) {
+		t.Error("17 should not be 'divisible'")
+	}
+	bigCommonMultiple := int64(1)
+	for i := int64(2); i <= 20; i++ {
+		bigCommonMultiple *= i
+	}
+	if !divisible(bigCommonMultiple) {
+		t.Error("%d should be 'divisible'", bigCommonMultiple)
+	}
+}

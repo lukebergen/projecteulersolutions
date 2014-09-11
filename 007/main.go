b280416ea283b22f0c214fd529602b7e79df19e3
Luke Bergen <LukeABergen@gmail.com>
2014-09-10 21:42:30 -0400

    Adding problem 7

--- 007/main.go
+++ 007/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println("solution")
+	c := 1
+	var result int64
+	for i := int64(3); true; i += 2 {
+		if isPrime(i) {
+			c++
+		}
+
+		if c == 10001 {
+			result = i
+			break
+		}
+	}
+	fmt.Println("result: %d", result)
+}
+
+func isPrime(n int64) bool {
+	start := int64(2)
+	end := int64(math.Ceil(math.Sqrt(float64(n))))
+	for i := start; i <= end; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
 }
